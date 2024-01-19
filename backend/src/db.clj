9dcaf0748f1316ca1ed2f731812a7324881cbf8a
Dirk <brigadirk@proton.me>
2024-01-19 17:40:40 +0100

    database

--- backend/src/db.clj
+++ backend/src/db.clj
@@ -1,7 +1,13 @@
+;; (def db-spec {:dbtype "postgresql"
+;;               :dbname "basedguide"
+;;               :user "dirk"
+;;               :password "database"})
+
 (ns db
-  (:require [next.jdbc :as jdbc]))
+  (:require [next.jdbc :as jdbc]
+            [clojure.java.io :as io]))
 
 (def db-spec {:dbtype "postgresql"
-              :dbname "basedguide"
-              :user "dirk"
-              :password "database"})
+              :dbname (System/getenv "DB_NAME")
+              :user (System/getenv "DB_USER")
+              :password (System/getenv "DB_PASSWORD")})
